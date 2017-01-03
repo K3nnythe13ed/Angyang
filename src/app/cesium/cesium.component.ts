4c04c77e977ffe1153e1dbd86f3f7636d4173d7e
K3nnythe13d <thegodlikez@web.de>
2017-01-03 13:37:25 +0100

    adding pins to cesium map

--- src/app/cesium/cesium.component.ts
+++ src/app/cesium/cesium.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, OnInit, Renderer, ViewChild } from '@angular/core';
-
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import * as elasticsearch from 'elasticsearch';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 /**
  * This is my cesium comment
  */
@@ -12,24 +12,94 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 export class CesiumComponent implements OnInit {
   @ViewChild('cesiumContainer') cesiumContainer: ElementRef;
   cesiumViewer: any;
+  private _client: elasticsearch.Client
+  pinBuilder: any;
 
   constructor(public element: ElementRef, private renderer: Renderer) {
     Cesium.BingMapsApi.defaultKey = 'AroazdWsTmTcIx4ZE3SIicDXX00yEp9vuRZyn6pagjyjgS-VdRBfBNAVkvrucbqr';
     (<any>window).CESIUM_BASE_URL = '/assets/Cesium';
+
+
+    this._client = new elasticsearch.Client({
+      host: 'localhost:9200'
+    });
   }
 
   ngAfterViewInit() {
-    this.cesiumViewer = new Cesium.Viewer(this.cesiumContainer.nativeElement);
+
+
     var terrainProvider = new Cesium.CesiumTerrainProvider({
       url: '//assets.agi.com/stk-terrain/world',
       requestWaterMask: true
     });
 
     this.cesiumViewer.terrainProvider = terrainProvider;
-  }
+    var viewer = this.cesiumViewer;
+    let marker = this.newMarkerOnMap;
+    let pinbuilder = this.pinBuilder;
+    this._client.search({
+      index: 'logstash-*',
+      type: 'warehouse',
+      size: 1000,
+      body: {
+        "sort": {
+          "properties.LocID": { "order": "desc" }
+        }
+      }
 
+    }, function (error, response) {
+      if (error) {
+        console.error('elasticsearch cluster is down!');
+      } else {
+        console.log(viewer)
+        response.hits.hits.forEach((hit) => {
+          marker(hit, viewer, pinbuilder);
+        })
+        console.log('All is well');
+
+      }
+    });
+
+
+
+  }
+  newMarkerOnMap(hit, viewer, pinBuilder) {
+
+    var markerHeight = Math.max(Math.min(hit._source.properties.Exp_TIV / 200000, 100), 20);
+
+    viewer.entities.add({
+      id: hit._source.properties.LocID,
+      name: hit._source.properties.AccountName,
+      position: Cesium.Cartesian3.fromDegrees(hit._source.geometry.coordinates[0], hit._source.geometry.coordinates[1], 200.0),
+      billboard: {
+        image: pinBuilder.fromColor(Cesium.Color.ROYALBLUE, markerHeight).toDataURL(),
+        verticalOrigin: Cesium.VerticalOrigin.BOTTOM
+      },
+      description: '\
+                        <p>\
+                        Location: '+ hit._source.properties.AccountName + ' \
+                        </p>\
+                        <p>\
+                        Location ID: '+ hit._source.properties.LocID + '\
+                        </p>\
+                        <p>\
+                        Exposure: '+ hit._source.properties.Exp_TIV + '\
+                        </p>\
+                        <p>\
+                        Coordinates: '+ hit._source.geometry.coordinates[0] + ', ' + hit._source.geometry.coordinates[1] + '\
+                        </p>\
+                        <p>\
+                        Risk Score: '+ hit._source.properties.MR_RISK_SCORE + '\
+                        </p>'
+    });
+
+
+  }
   ngOnInit() {
-
+    this.pinBuilder = new Cesium.PinBuilder();
+    this.cesiumViewer = new Cesium.Viewer(this.cesiumContainer.nativeElement);
   }
+
+
 
 }
